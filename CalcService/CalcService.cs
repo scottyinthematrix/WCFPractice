2107e5eb42ca7115eec24c8eada965fd487a9daa
unknown <scotty.gang.yang@CDC312800.gdnchina.com>
2013-03-07 14:40:36 +0800

    service and IISHost created

---
+++ CalcService/CalcService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ScottyApps.WCFPractice.CalcService
+{
+    public class CalcService : ICalcService
+    {
+        public Complex Add(Complex x, Complex y)
+        {
+            return new Complex(x.Real + y.Real, x.Imaginary + y.Imaginary);
+        }
+
+        public Complex Subtract(Complex x, Complex y)
+        {
+            return new Complex(x.Real - y.Real, x.Imaginary - y.Imaginary);
+        }
+
+        public Complex Multiply(Complex x, Complex y)
+        {
+            return new Complex(x.Real * y.Real - x.Imaginary * y.Imaginary, x.Real * y.Imaginary + x.Imaginary * y.Real);
+        }
+
+        public Complex Devide(Complex x, Complex y)
+        {
+            throw new NotImplementedException("too complex to remember...");
+        }
+    }
+}
---
+++ CalcService/ICalcService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ScottyApps.WCFPractice.CalcService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        Complex Add(Complex x, Complex y);
+
+        [OperationContract]
+        Complex Subtract(Complex x, Complex y);
+
+        [OperationContract]
+        Complex Multiply(Complex x, Complex y);
+
+        [OperationContract]
+        Complex Devide(Complex x, Complex y);
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Complex
+    {
+        public Complex(double real, double imaginary)
+        {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+        [DataMember]
+        public double Real { get; set; }
+
+        [DataMember]
+        public double Imaginary { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} + i{1}", Real, Imaginary);
+        }
+    }
+}
